9154a533e9a64036ea974d1b9539948b21cd75d0
ShinDongJae <sdhsdj6450@gmail.com>
2023-04-05 16:12:18 +0900

    feat: Header Layout and Router hook

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,3 +1,28 @@
-export default function Header() {
-  return <div>Header</div>;
-}
+import styled from "styled-components";
+import { useRouter } from "../hooks/useRouter";
+
+const HeaderWapper = styled.header`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 3rem;
+  border: 1px solid blue;
+  cursor: pointer;
+`;
+
+const Logo = styled.span`
+  font-size: 1.5rem;
+  font-weight: 800;
+`;
+
+const Header = () => {
+  const { routeTo } = useRouter();
+
+  return (
+    <HeaderWapper onClick={() => routeTo("/")}>
+      <Logo>Worker Well-Being</Logo>
+    </HeaderWapper>
+  );
+};
+
+export default Header;
