8c3679c785b6df6e49097cf71fde3a71721c55ce
dlawjdghks11 <85840643+dlawjdghks11@users.noreply.github.com>
2023-04-29 23:39:47 +0900

    Feat: Answer페이지 생성

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,14 @@ import React from 'react';
 import './App.css';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
+import Answer from './pages/Answer';
 
 function App() {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/answer" element={<Answer />} />
       </Routes>
     </Router>
   );
